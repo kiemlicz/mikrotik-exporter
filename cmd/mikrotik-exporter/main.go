714fe53800edd27d4d76166d6ccbd3364f2be241
stanislaw_jakiel <stanislaw.dev@gmail.com>
2025-04-29 22:04:13 +0200

    experimenting

--- cmd/mikrotik-exporter/main.go
+++ cmd/mikrotik-exporter/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/promslog"
+)
 
+var ()
+
+func main() {
+	promslogConfig := &promslog.Config{}
+	logger := promslog.New(promslogConfig)
+	logger.Info("Starting Mikrotik Exporter")
+
+	prometheus.MustRegister(versioncollector.NewCollector("mikrotik_exporter"))
+
+	//srv := &http.Server{}
+	//if err := web.ListenAndServe(srv, toolkitFlags, logger); err != nil {
+	//	logger.Error("Error starting HTTP server", "err", err)
+	//	os.Exit(1)
+	//}
 }
