13714bef94d25c0217e4212b52c8b72b6b41f3e0
stanislaw_jakiel <stanislaw.dev@gmail.com>
2025-05-02 15:49:02 +0200

    Proper place to define collections

--- internal/collector/collector.go
+++ internal/collector/collector.go
@@ -10,23 +10,21 @@ import (
 	"net/http"
 )
 
-var (
-	log = logger.Log
-)
-
 func Start() {
-	var c = []prometheus.Collector{}
+	var c []prometheus.Collector
 	var metricsPath = viper.GetString("metrics.path")
 	var addr = fmt.Sprintf("%s:%d", viper.GetString("metrics.host"), viper.GetInt("metrics.port"))
-	var metrics = viper.Get("metrics.collect").([]interface{})
 	registry := prometheus.NewRegistry()
-	for _, e := range metrics {
-		m := e.(map[string]interface{})
-		switch m["metric_type"] {
-		case "gauge":
-			log.Info("gauge")
-		default:
-			log.Warnf("Unknown metric type: %s", m["metric_type"])
+
+	targets := viper.GetStringMap("targets")
+	for _, config := range targets { // targetIP, config
+		if targetConfig, ok := config.(map[string]interface{}); ok {
+			collect := targetConfig["collect"].([]interface{})
+			for _, item := range collect {
+				if metric, ok := item.(map[string]interface{}); ok {
+					createCollector(metric, &c)
+				}
+			}
 		}
 	}
 
@@ -44,6 +42,43 @@ func Start() {
 				Registry:          registry,
 			}),
 	)
+	logger.Log.Infof("Starting metrics server on %s", addr)
 	// To test: curl -H 'Accept: application/openmetrics-text' localhost:8080/metrics
-	log.Fatalln(http.ListenAndServe(addr, nil))
+	logger.Log.Fatalln(http.ListenAndServe(addr, nil))
+}
+
+func createCollector(m map[string]interface{}, colls *[]prometheus.Collector) {
+	switch m["metric_type"] {
+	case "gauge":
+		rawLabels, ok := m["labels"].([]interface{})
+		if !ok {
+			logger.Log.Warnf("Invalid labels format for metric: %s", m["name"])
+			return
+		}
+		labels := make([]string, len(rawLabels))
+		for i, label := range rawLabels {
+			if strLabel, ok := label.(string); ok {
+				labels[i] = strLabel
+			} else {
+				logger.Log.Warnf("Invalid label type for metric: %s", m["name"])
+				return
+			}
+		}
+		g := prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: m["name"].(string),
+			},
+			labels,
+		)
+		*colls = append(*colls, g)
+	default:
+		logger.Log.Warnf("Unknown metric type: %s", m["metric_type"])
+	}
+}
+
+// Collect targets:
+// on scrape but
+// no more often than given frequency
+func Collect() {
+
 }
