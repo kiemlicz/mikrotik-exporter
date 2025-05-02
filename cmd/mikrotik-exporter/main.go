66bdd11cef25fa600ba540bee2675621dab3d8bc
stanislaw_jakiel <stanislaw.dev@gmail.com>
2025-05-02 14:05:38 +0200

    Central log setup Experimenting with expoerter

--- cmd/mikrotik-exporter/main.go
+++ cmd/mikrotik-exporter/main.go
@@ -2,25 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"mikrotik-exporter/internal/collector"
+	"mikrotik-exporter/internal/logger"
 
 	"strings"
 )
 
-var ()
-
 func main() {
 	setupConfig()
-	var log = setupLogger()
-	log.Infof("Mikrotik Exporter starting...")
+	logger.Setup()
+	logger.Log.Infof("Mikrotik Exporter starting...")
 
-	//srv := &http.Server{}
-	//if err := web.ListenAndServe(srv, toolkitFlags, logger); err != nil {
-	//	logger.Error("Error starting HTTP server", "err", err)
-	//	os.Exit(1)
-	//}
+	collector.Start()
 }
 
 func setupConfig() {
@@ -40,20 +35,4 @@ func setupConfig() {
 	if err != nil {
 		panic(fmt.Errorf("fatal error config file: %s", err))
 	}
-}
-
-func setupLogger() *logrus.Logger {
-	var log = logrus.New()
-	logLevel := viper.GetString("log.level")
-	level, err := logrus.ParseLevel(strings.ToLower(logLevel))
-	if err != nil {
-		log.Warnf("Invalid log level in config: %s. Using 'info'.", logLevel)
-		level = logrus.InfoLevel
-	}
-
-	log.SetLevel(level)
-	log.SetFormatter(&logrus.TextFormatter{
-		FullTimestamp: true,
-	})
-	return log
 }
--- internal/collector/collector.go
+++ internal/collector/collector.go
@@ -1,28 +1,49 @@
 package collector
 
 import (
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/spf13/viper"
+	"mikrotik-exporter/internal/logger"
 	"net/http"
 )
 
 var (
-	customMetric = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "custom_metric_name",
-			Help: "Description of the custom metric",
-		},
-		[]string{"label1", "label2"},
-	)
+	log = logger.Log
 )
 
-func init() {
-	// Register the custom metric with Prometheus
-	prometheus.MustRegister(customMetric)
-}
+func Start() {
+	var c = []prometheus.Collector{}
+	var metricsPath = viper.GetString("metrics.path")
+	var addr = fmt.Sprintf("%s:%d", viper.GetString("metrics.host"), viper.GetInt("metrics.port"))
+	var metrics = viper.Get("metrics.collect").([]interface{})
+	registry := prometheus.NewRegistry()
+	for _, e := range metrics {
+		m := e.(map[string]interface{})
+		switch m["metric_type"] {
+		case "gauge":
+			log.Info("gauge")
+		default:
+			log.Warnf("Unknown metric type: %s", m["metric_type"])
+		}
+	}
 
-func StartMetricsServer() {
-	// Expose metrics on /metrics endpoint
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":8080", nil)
+	registry.MustRegister(
+		collectors.NewGoCollector(),
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
+	)
+	registry.MustRegister(c...)
+
+	http.Handle(
+		metricsPath, promhttp.HandlerFor(
+			registry,
+			promhttp.HandlerOpts{
+				EnableOpenMetrics: true,
+				Registry:          registry,
+			}),
+	)
+	// To test: curl -H 'Accept: application/openmetrics-text' localhost:8080/metrics
+	log.Fatalln(http.ListenAndServe(addr, nil))
 }
